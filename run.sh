51ee1cbab7f996424bdb6d10d4f1a2d284b9a663
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-02-29 16:46:34 -0300

    updating dokcer compose file for production and changing run.sh script
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- run.sh
+++ run.sh
@@ -1,36 +1,21 @@
-#!/bin/bash
+# Initialize the compose_file variable with default docker-compose file
+compose_file="docker-compose.yml"
 
-# Initialize environment file variable
-env_file=""
-
-# Function to display usage
-usage() {
-    echo "Usage: $0 --dev|--prod --service 'service1 service2 ...'"
-    exit 1
-}
-
-# Process command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --dev) env_file=".env.development";;
-        --prod) env_file=".env.production";;
-        --service) services="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; usage ;;
+        --prod)
+            env_file=".env.production"
+            compose_file="docker-compose.prod.yml"
+            ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-# Validate environment file
 if [ -z "$env_file" ]; then
     echo "Please specify --dev or --prod."
     exit 1
 fi
 
-# Build and start services
-if [ -z "$services" ]; then
-    # Build and start all services if none are specified
-    docker-compose --env-file "$env_file" up --build --force-recreate
-else
-    # Build and start specified services
-    docker-compose --env-file "$env_file" up --build --force-recreate $services
-fi
+docker-compose -f $compose_file --env-file $env_file up --build --force-recreate
