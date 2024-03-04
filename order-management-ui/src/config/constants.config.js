070579579c673aa1a4430f072cb5036fad121689
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-03-03 22:59:46 -0300

    updates in UI and fixing errors
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- order-management-ui/src/config/constants.config.js
+++ order-management-ui/src/config/constants.config.js
@@ -1,13 +1,10 @@
-export const { VITE_NODE_ENV, VITE_RESTAURANT_ORDER_SYSTEM_DOMAIN } =
-  import.meta.env;
-
 export const SERVICE_NAME = "Warehouse";
 export const KITCHEN_API_PATH_SUFFIX = "api/v1/kitchen";
 export const WAREHOUSE_API_PATH_SUFFIX = "api/v1/warehouse";
 
-console.log(VITE_NODE_ENV);
+const { MODE, VITE_RESTAURANT_ORDER_SYSTEM_DOMAIN } = import.meta.env;
 
 export const KITCHEN_API_BASE_URL =
-  VITE_NODE_ENV === "production"
+  MODE === "production"
     ? `http://${VITE_RESTAURANT_ORDER_SYSTEM_DOMAIN}/${KITCHEN_API_PATH_SUFFIX}`
     : `http://localhost:8080/${KITCHEN_API_PATH_SUFFIX}`;
--- order-management-ui/vite.config.js
+++ order-management-ui/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 
 // Determine the current environment based on NODE_ENV, defaulting to 'development'
-const env = process.env.VITE_NODE_ENV || "development";
+const env = process.env.NODE_ENV || "development";
 
 // Manually specify the path to the external .env file
 const envPath = path.resolve(process.cwd(), `../.env.${env}`);
@@ -13,24 +13,16 @@ const envPath = path.resolve(process.cwd(), `../.env.${env}`);
 // Load environment variables from the corresponding .env file
 dotenv.config({ path: envPath });
 
-// Filter out VITE_ prefixed variables and prepare them for the define option
-const viteEnvVariables = Object.entries(process.env).reduce((acc, [key, value]) => {
-  if (key.startsWith('VITE_')) {
-    // Vite replaces `import.meta.env` with the actual environment variables
-    // We need to stringify the values for proper inclusion
-    acc[`import.meta.env.${key}`] = JSON.stringify(value);
-  }
-  return acc;
-}, {});
-
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host: '0.0.0.0',
-    port: 3000
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
   },
-  define: viteEnvVariables,
   plugins: [react()],
-})
+  define: {
+    "import.meta.env.VITE_RESTAURANT_ORDER_SYSTEM_DOMAIN": JSON.stringify(
+      process.env.RESTAURANT_ORDER_SYSTEM_DOMAIN
+    ),
+  },
+});
