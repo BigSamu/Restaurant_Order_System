ac02d4bca5bf6977631a7acd4c3d162090e9b256
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-03-03 23:10:33 -0300

    updates in UI and fixing errors
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- order-management-ui/vite.config.js
+++ order-management-ui/vite.config.js
@@ -10,6 +10,8 @@ const env = process.env.NODE_ENV || "development";
 // Manually specify the path to the external .env file
 const envPath = path.resolve(process.cwd(), `../.env.${env}`);
 
+console.log(envPath)
+
 // Load environment variables from the corresponding .env file
 dotenv.config({ path: envPath });
 
